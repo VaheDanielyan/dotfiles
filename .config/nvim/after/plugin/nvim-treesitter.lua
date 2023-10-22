f62ef605e139de2691d36a6ed608e692d4f3b3b9
Vahe Danielyan <vahe.danielyan@aerodynamics.am>
2023-10-22 18:32:31 +0400

    Miscellaneous

--- .config/nvim/after/plugin/nvim-treesitter.lua
+++ .config/nvim/after/plugin/nvim-treesitter.lua
@@ -3,8 +3,8 @@ require'nvim-treesitter.configs'.setup {
     ensure_installed = {"arduino", "bash", "c", "cpp", "cmake", "comment",
                         "cuda", "diff", "dockerfile", "devicetree", "git_config",
                         "git_rebase", "gitattributes", "gitcommit", "gitignore",
-                        "lua", "markdown", "ninja", "proto",
-                        "sql", "vim", "vimdoc", "yaml", "hjson", "rust", "go"},
+                        "lua", "markdown", "ninja", "proto", "yaml",
+                        "sql", "vim", "vimdoc", "hjson", "rust", "go"},
     highlight = {
         enable = true,
     },
